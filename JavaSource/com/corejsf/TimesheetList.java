f9e34e0c46d4ae9ac1b4ea0ee5176eb77aefbc22
DESKTOP-O4KO1CS\Phili <philip_choi96@hotmail.com>
2017-11-08 14:53:38 -0800

    Change

---
+++ JavaSource/com/corejsf/TimesheetList.java
@@ -0,0 +1,40 @@
+package com.corejsf;
+
+import java.util.List;
+
+import ca.bcit.infosys.employee.Employee;
+import ca.bcit.infosys.timesheet.Timesheet;
+import ca.bcit.infosys.timesheet.TimesheetCollection;
+
+/**
+ * TimesheetList class that implements TimesheetCollection interface.
+ * @author Philip
+ * @version 1.0
+ */
+public class TimesheetList implements TimesheetCollection {
+
+	@Override
+	public List<Timesheet> getTimesheets() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Timesheet> getTimesheets(Employee e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Timesheet getCurrentTimesheet(Employee e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String addTimesheet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
